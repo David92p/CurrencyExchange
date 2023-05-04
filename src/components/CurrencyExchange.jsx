bab44674eabc9dd954261e1a5d2008d0bfa1b6f0
David92p <dave.panetta92@gmail.com>
2023-05-04 15:49:09 +0200

    fixed select converter

--- src/components/CurrencyExchange.jsx
+++ src/components/CurrencyExchange.jsx
@@ -3,32 +3,56 @@
 // import { useEffect, useState } from "react";
 
 const CurrencyExchange = ({
+  converter,
   options,
   currency,
   amount,
   handleCurrencyChange,
   handleAmountChange,
 }) => {
+  //console.log(`la conversione è ${conversion}`);
   return (
-    <div>
-      <select name="currency" value={currency} onChange={handleCurrencyChange}>
-        {Object.keys(options).map((key) => {
-          return (
-            <option value={key} key={key}>
-              {options[key]}
-            </option>
-          );
-        })}
-      </select>
-      <input
-        type="number"
-        min="0"
-        max="100000"
-        step="1"
-        value={amount}
-        onChange={handleAmountChange}
-      />
-    </div>
+    <>
+      <label>
+        {converter
+          ? "Choose the currency and the amount to convert"
+          : "Choose the currency to receive"}
+      </label>
+      <div>
+        <select
+          name="currency"
+          value={currency}
+          onChange={(e) => handleCurrencyChange(e, converter)}
+        >
+          {Object.keys(options).map((key) => {
+            return (
+              <option value={key} key={key}>
+                {options[key]}
+              </option>
+            );
+          })}
+        </select>
+        {converter ? (
+          <input
+            type="number"
+            min={1}
+            max={1000000}
+            step="1"
+            value={amount}
+            onChange={handleAmountChange}
+          />
+        ) : (
+          <input
+            disabled
+            type="number"
+            min={1}
+            max={1000000}
+            step="1"
+            value={amount}
+          />
+        )}
+      </div>
+    </>
   );
 };
 
