7b5758713cd72c2a9e28e317fa257651079ce804
David92p <dave.panetta92@gmail.com>
2023-05-08 15:55:59 +0200

    Updated wallet component

--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-const Wallet = () => {
+import { SlWallet } from "react-icons/sl";
+import symbols from "../assets/symbols";
+
+const Wallet = ({ amount, base, date, rates }) => {
   return (
-    <div className='wallet'>
-      <h1>WALLET</h1>
+    <div className="container-wallet">
+      <div className="header-wallet">
+        <h4>Currency Update To:</h4>
+        <h2>{date}</h2>
+      </div>
+      <div className="main-wallet">
+        <div className="container-amount">
+          <SlWallet style={{ fontSize: "1.5em" }} />
+          {amount + " "}
+          {base}
+          <img
+            src={symbols[base]}
+            alt={base}
+            style={{ heigth: "50px", width: "50px" }}
+          />
+        </div>
+        <div className="container-currencies"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Wallet
+export default Wallet;
