22449e91698a098f9c6f81a7c4159e6e7c89bc24
David92p <dave.panetta92@gmail.com>
2023-05-08 15:54:24 +0200

    Added symbol file

---
+++ src/assets/symbols.js
@@ -0,0 +1,67 @@
+import aud from "./images/aud.png";
+import bgn from "./images/bgn.png";
+import brl from "./images/brl.png";
+import cad from "./images/cad.png";
+import chf from "./images/chf.png";
+import cny from "./images/cny.png";
+import czk from "./images/czk.png";
+import dkk from "./images/dkk.png";
+import eur from "./images/eur.png";
+import gbp from "./images/gbp.png";
+import hkd from "./images/hkd.png";
+import huf from "./images/huf.png";
+import idr from "./images/idr.png";
+import ils from "./images/ils.png";
+import inr from "./images/inr.png";
+import isk from "./images/isk.png";
+import jpy from "./images/jpy.png";
+import krw from "./images/krw.png";
+import mxn from "./images/mxn.png";
+import myr from "./images/myr.png";
+import nok from "./images/nok.png";
+import nzd from "./images/nzd.png";
+import php from "./images/php.png";
+import pln from "./images/pln.png";
+import ron from "./images/ron.png";
+import sek from "./images/sek.png";
+import sgd from "./images/sgd.png";
+import thb from "./images/thb.png";
+import TRY from "./images/try.png";
+import usd from "./images/usd.png";
+import zar from "./images/zar.png";
+
+const symbols = {
+  AUD: aud,
+  BGN: bgn,
+  BRL: brl,
+  CAD: cad,
+  CHF: chf,
+  CNY: cny,
+  CZK: czk,
+  DKK: dkk,
+  EUR: eur,
+  GBP: gbp,
+  HKD: hkd,
+  HUF: huf,
+  IDR: idr,
+  ILS: ils,
+  INR: inr,
+  ISK: isk,
+  JPY: jpy,
+  KRW: krw,
+  MXN: mxn,
+  MYR: myr,
+  NOK: nok,
+  NZD: nzd,
+  PHP: php,
+  PLN: pln,
+  RON: ron,
+  SEK: sek,
+  SGD: sgd,
+  THB: thb,
+  TRY: TRY,
+  USD: usd,
+  ZAR: zar,
+};
+
+export default symbols;
