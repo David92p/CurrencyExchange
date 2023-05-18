5d20dd8187095c92b46e0c283bf5dbf61a226638
David92p <dave.panetta92@gmail.com>
2023-05-18 16:42:31 +0200

    Modified btn positioning inside the Chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,9 +9,8 @@ import {
   Tooltip,
   Filler,
   Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
-
+} from "chart.js";
+import { Line } from "react-chartjs-2";
 
 ChartJS.register(
   CategoryScale,
@@ -24,55 +23,54 @@ ChartJS.register(
   Legend
 );
 
-
-
-const Chart = ({period, values}) => {
-  console.log(values);
- 
-  
+const Chart = ({ period, values, trend, leftCurrency, rightCurrency }) => {
   const options = {
     elements: {
       point: {
         radius: 4,
         pointStyle: "circle",
         backgroundColor: "white",
-        borderColor: 'rgba(23, 177, 105, 1)'
+        borderColor: "rgba(23, 177, 105, 1)",
       },
       line: {
         backgroundColor: "white",
-        borderWidth: 3
-      }
-    }
-     
+        borderWidth: 3,
+      },
+    },
   };
-  
+
   const data = {
     labels: period, // date temporali su asse X
     datasets: [
       {
         fill: true,
-        label: 'Market trend',
+        label: "Market trend",
         data: values, // valori di cambio su asse Y
-        borderColor: 'rgba(23, 177, 105, 1)',
-        backgroundColor: 'rgba(23, 177, 105, 0.1)',
-      }
+        borderColor: "rgba(23, 177, 105, 1)",
+        backgroundColor: "rgba(23, 177, 105, 0.1)",
+      },
     ],
   };
 
   /////////////////////////////////////////////////////////////////////////////
   return (
-    <div className='container-chart'>
-      <div className='btn-chart'>
-        <button>3 MESI</button>
-        <button>MESE </button>
-        <button>ULTIMO</button>
+    <div className="container-chart">
+      <div className="header-chart">
+        <div>Change {trend}</div>
+        <div>
+          {leftCurrency} / {rightCurrency}
+        </div>
       </div>
-      <div className='chart'>
-        <Line data = {data} options={options} />
-        
+      <div className="main-chart">
+        <Line data={data} options={options} />
+        <div className="main-btn">
+          <button className="btn-chart">Week</button>
+          <button className="btn-chart">Month </button>
+          <button className="btn-chart">3 Months</button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chart
+export default Chart;
